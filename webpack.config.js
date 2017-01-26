dfcc5e42d1a74486909f64c5398a5c55d7da5cdd
Jimmy Sinn <jimmyltsinn@gmail.com>
2017-01-26 18:34:31 +0800

    Initial commit: Starter code from tutorial

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: './dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
+  'plugins': [
+    // new webpack.optimize.UglifyJsPlugin({
+    //     compress: {
+    //         warnings: false,
+    //     },
+    //     output: {
+    //         comments: false,
+    //     },
+    // }),
+    new HtmlWebpackPlugin({
+      title: 'React Tic-tac-toe Example',
+      template: './src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        // NODE_ENV: JSON.stringify('production')
+      }
+    }),
+  ]
+};

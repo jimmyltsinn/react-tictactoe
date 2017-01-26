23a72959d643bbae73c57e0fa356f92896badadf
Jimmy Sinn <jimmyltsinn@gmail.com>
2017-01-26 19:42:49 +0800

    Use immutable.js; Add history and time travel

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Immutable from 'immutable';
 
 require("./style.css");
 
@@ -17,32 +18,14 @@ Square.propTypes = {
 };
 
 class Board extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) return;
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
+    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
   }
 
   render() {
-    const status = 'Next player: ' + (calculateWinner(this.state.squares) ? '-' : this.state.xIsNext ? 'X' : 'O');
+    // const status = 'Next player: ' + (calculateWinner(this.props.squares) ? '-' : this.props.xIsNext ? 'X' : 'O');
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -63,19 +46,73 @@ class Board extends React.Component {
   }
 }
 
+Board.propTypes = {
+  squares: React.PropTypes.array,
+  onClick: React.PropTypes.func
+};
+
+
 class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      history: Immutable.List([{
+        squares: Array(9).fill(null),
+        xIsNext: true,
+      }]).toArray()
+    };
+  }
+
   render() {
+    const history = this.state.history;
+    console.log(history[0]);
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status = winner ? ('Winner: ' + winner) : 'Next Player: ' + (current.xIsNext ? 'X' : 'O');
+
+    const moves = history.map((step, id) => {
+      const desc = id ? 'Move #' + id : 'Game Start';
+      return (
+        <li key={id}>
+          <a href="#" onClick={() => this.jumpTo(id)}>{desc}</a>
+        </li>
+      );
+    });
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={i => this.handleClick(i)}
+            />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
+  }
+
+  jumpTo(i) {
+    this.setState({
+      history: this.state.history.slice(0, i + 1)
+    });
+  }
+
+  handleClick(i) {
+    const current = this.state.history[this.state.history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) return;
+    squares[i] = current.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: Immutable.List(this.state.history).push({
+        squares: squares,
+        xIsNext: !current.xIsNext
+      }).toArray()
+    });
   }
 }
 
